ea19b6c3c7c94cbe5364ba71c334c04b1c267159
Oleg_S <hohol888ua@gmail.com>
2020-12-13 15:02:13 +0200

    add section slider

---
+++ bislite/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+   
+   $('.slider__inner').slick({
+      arrows: false,
+      dots: true,
+      speed: 600,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+   });
+
+ });
